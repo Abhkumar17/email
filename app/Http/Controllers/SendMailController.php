298b799702516471ef4b170df0d422b61ef28959
Abhkumar17 <abhi.bsi001@gmail.com>
2022-11-29 14:46:42 +0530

    first commit

---
+++ app/Http/Controllers/SendMailController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use App\Models\User;
+use App\Mail\SendMail;
+
+class SendMailController extends Controller
+{
+    
+    public function index()
+    {
+        $data = ["name"=>"abhishek"];
+        $user['to'] = 'abhkumar17@gmail.com';
+        mail::send('sendDemoMail', $data, function ($messages) use ($user) {
+            $messages->to($user['to']);
+            $messages->subject('hello');
+        });
+      
+    }
+
+    public function show()
+    {
+        return view('email_form');
+    }
+
+    public function autocomplete(Request $request)
+    {
+        $data = User::select("email as value", "id")
+                    ->where('email', 'LIKE', $request->get('search'). '%')
+                    ->get();
+
+        return response()->json($data);
+    }
+}
