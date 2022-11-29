48ba28b3eab3ea894209c09ddde6bf1b8654bd9c
Abhkumar17 <abhi.bsi001@gmail.com>
2022-11-29 18:24:06 +0530

    live searching in jquery

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SendMailController;
+use App\Http\Controllers\LiveSearchController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('mail-send', [SendMailController::class, 'index']);
 Route::get('showForm', [SendMailController::class, 'show']);
 Route::get('autocomplete', [SendMailController::class,'autocomplete'])->name('autocomplete');
 
 Route::post('/emailsend', [SendMailController::class,'emailsend'])->name('emailsend');
+
+Route::get('/livesearch', [LiveSearchController::class, 'index_1']);
+Route::get('/action', [LiveSearchController::class, 'action'])->name('action');
